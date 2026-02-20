d5f094abc06c08d6c358e9685395354a36a41670
Nasir-2006 <n9750998@gmail.com>
2026-02-20 15:27:11 +0500

    LINEAR SEARCH WITH RANGE

--- LINEAR_SEARCH.java
+++ LINEAR_SEARCH.java
@@ -7,14 +7,20 @@ public class LINEAR_SEARCH {
         int elmnt = sc.nextInt();
         System.out.println("Enter array Elements:");
         int[] arr = new int[elmnt];
-        for(int i = 0 ; i < arr.length;i++)
+         for(int i = 0 ; i < arr.length;i++)
         {
             arr[i] = sc.nextInt();
         }
+        System.out.println("-------------------");
+        System.out.println("Enter Start range:");
+        int start  = sc.nextInt();
+        System.out.println("Enter End range:");
+        int end  = sc.nextInt();
+       
         System.out.println("Enter number you want to Search:");
         int search = sc.nextInt();
         boolean isfound = false;
-        for(int j = 0;j<arr.length;j++)
+        for(int j = start;j<=end;j++)
         {
             // System.out.println(arr);
             if(search == arr[j])
